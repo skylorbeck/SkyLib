68996c7bebc779136d5c8d09b635b5e5f1835279
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2021-08-17 17:05:52 -0700

    tool abstraction moved from sentimentality

---
+++ src/main/java/website/skylorbeck/minecraft/skylorlib/tools/ExtraPickaxeItem.java
@@ -0,0 +1,10 @@
+package website.skylorbeck.minecraft.skylorlib.tools;
+
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ToolMaterial;
+
+public class ExtraPickaxeItem extends PickaxeItem {//Pickaxe is protected for some reason
+    public ExtraPickaxeItem(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
