3044a1ed6626f7f253e12747d1cf657c0123baa8
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2022-02-08 11:19:54 -0800

    added item transfer code

--- src/main/java/website/skylorbeck/minecraft/skylorlib/storage/StorageUtils.java
+++ src/main/java/website/skylorbeck/minecraft/skylorlib/storage/StorageUtils.java
@@ -1,9 +1,12 @@
 package website.skylorbeck.minecraft.skylorlib.storage;
 
+import net.minecraft.inventory.Inventory;
+import net.minecraft.inventory.SidedInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.Direction;
 
 public class StorageUtils {
     public static void readNbt(NbtCompound nbt, DefaultedList<ItemStack> stacks) {
@@ -51,5 +54,45 @@ public class StorageUtils {
         }
 
         return nbt;
+    }
+
+    public static ItemStack transfer(Inventory to, ItemStack stack, int slot, Direction dir) {
+        ItemStack itemStack = to.getStack(slot);
+        if (canInsert(to, stack, slot, dir)) {
+            int j;
+            boolean bl = false;
+            if (itemStack.isEmpty()) {
+                to.setStack(slot, stack);
+                stack = ItemStack.EMPTY;
+                bl = true;
+            } else if (canMergeItems(itemStack, stack)) {
+                int i = stack.getMaxCount() - itemStack.getCount();
+                j = Math.min(stack.getCount(), i);
+                stack.decrement(j);
+                itemStack.increment(j);
+            }
+            if (bl) {
+                to.markDirty();
+            }
+        }
+        return stack;
+    }
+    public static boolean canInsert(Inventory inventory, ItemStack stack, int slot, Direction dir) {
+        if (!inventory.isValid(slot, stack)) {
+            return false;
+        }
+        return !(inventory instanceof SidedInventory) || ((SidedInventory)inventory).canInsert(slot, stack, dir);
+    }
+    public static boolean canMergeItems(ItemStack first, ItemStack second) {
+        if (!first.isOf(second.getItem())) {
+            return false;
+        }
+        if (first.getDamage() != second.getDamage()) {
+            return false;
+        }
+        if (first.getCount() > first.getMaxCount()) {
+            return false;
+        }
+        return ItemStack.areNbtEqual(first, second);
     }
 }
