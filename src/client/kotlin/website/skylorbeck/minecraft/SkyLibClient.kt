4dd974d66e0e9a39c8bb22afdcc08854ecc89e1a
Skylor Beck <42103122+skylorbeck@users.noreply.github.com>
2023-10-19 09:08:31 -0700

    1.20.2
    Signed-off-by: Skylor Beck <42103122+skylorbeck@users.noreply.github.com>

---
+++ src/client/kotlin/website/skylorbeck/minecraft/SkyLibClient.kt
@@ -0,0 +1,9 @@
+package website.skylorbeck.minecraft
+
+import net.fabricmc.api.ClientModInitializer
+
+object SkyLibClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/website/skylorbeck/minecraft/SkyLibDataGenerator.kt
@@ -0,0 +1,9 @@
+package website.skylorbeck.minecraft
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object SkyLibDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
